054b05d4818c9568128e0e225f6c73c4bfa0983b
Aranfyro <141035777+Aranfyro@users.noreply.github.com>
2024-09-08 14:17:33 +0300

    Add files via upload

---
+++ module_3_5.py
@@ -0,0 +1,15 @@
+# Самостоятельная работа по уроку "Рекурсия"
+# Задача "Рекурсивное умножение цифр":
+
+def get_multiplied_digits (number):
+    str_number = str(number)
+    first = int(str_number[0])
+    if (len(str_number)) <= 1:
+        return first
+    else:
+        return first * get_multiplied_digits(int(str_number[1:]))
+
+result = get_multiplied_digits(40203)
+print(result)
+
+
